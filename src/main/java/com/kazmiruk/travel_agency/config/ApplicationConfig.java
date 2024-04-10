c3ce11f0b49a95258a896f71895239d9c57863e8
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2024-04-10 17:24:26 +0300

    Add basic auth for all request

--- src/main/java/com/kazmiruk/travel_agency/config/ApplicationConfig.java
+++ src/main/java/com/kazmiruk/travel_agency/config/ApplicationConfig.java
@@ -1,13 +1,20 @@
 package com.kazmiruk.travel_agency.config;
 
 import com.github.javafaker.Faker;
+import com.kazmiruk.travel_agency.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository userRepository;
 
     @Bean
     public Faker faker() {
@@ -17,6 +24,12 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
 }
